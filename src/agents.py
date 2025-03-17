e680826ec23e895c194325b3cbe04b21fe9685f5
Aymane Lotfi <aymane.lotfi@student-cs.fr>
2025-03-17 14:12:42 +0100

    [Step 1] : fixed pickup

--- src/agents.py
+++ src/agents.py
@@ -33,7 +33,7 @@ class Drone(Agent):
                 else [self.pos]
             )
         )
-
+        print(f"Drone {self.unique_id} moving to {new_position}")
         # Move the agent to the new position
         self.model.grid.move_agent(self, new_position)
 
@@ -52,14 +52,16 @@ class Drone(Agent):
         wastes_at_position = [
             (waste_id, waste_pos)
             for waste_id, waste_pos in self.percepts["neighbor_wastes"]
-            if waste_pos == self.pos
         ]
-
         if wastes_at_position and self.knowledge["carried_waste_amount"] < 2:
             waste_id, _ = wastes_at_position[0]  # Get the first waste
             waste = self.model.get_agent_by_id(waste_id)
-            if waste:
+            if (
+                waste.waste_color == self.knowledge["carried_waste_type"]
+                or self.knowledge["carried_waste_type"] is None
+            ) and waste.waste_color <= self.knowledge["zone_type"]:
                 self.knowledge["carried_waste_amount"] += waste.weight
+                self.knowledge["carried_waste_type"] = waste.waste_color
                 self.model.grid.remove_agent(waste)
                 self.knowledge["actions"].append(f"picked waste {waste_id}")
                 print(f"Drone {self.unique_id} picked waste {waste.unique_id}")
@@ -69,7 +71,7 @@ class Drone(Agent):
     def drop_waste(self):
         # Only execute if carrying waste
         if self.knowledge["carried_waste_amount"] > 0:
-            self.knowledge["carried_waste_amount"] = 0
+            self.knowledge["carried_waste_amount"] -= 1
             self.knowledge["carried_waste_type"] = None
             self.model.add_agent(
                 Waste(self.model, self.knowledge["zone_type"] + 1), self.pos
@@ -96,25 +98,41 @@ class Drone(Agent):
         self.knowledge["actions"].append("transformed waste")
 
     def deliberate(self):
+        print(
+            f"Carried_waste_amount: {self.knowledge['carried_waste_amount'], self.knowledge['carried_waste_type']}"
+        )
+
         if self.knowledge["carried_waste_amount"] == 2:
+            print("Transforming waste")
             return "transform_waste"
         # First priority: Drop waste if in drop zone and carrying waste
         if (
             self.knowledge["in_drop_zone"]
             and self.knowledge["carried_waste_amount"] > 0
         ):
-            if self.knowledge["carried_waste_type"] == self.knowledge["zone_type"] + 1:
+            if self.knowledge["carried_waste_type"] == (
+                self.knowledge["zone_type"] + 1
+            ):
+                print("Dropping waste")
                 return "drop_waste"
 
         # Second priority: Pick waste if at same position as waste and has capacity
-        for _, waste_pos in self.percepts["neighbor_wastes"]:
-            if waste_pos == self.pos:
-                return "pick_waste"
+        filtered_wastes = []
+        for waste_id, waste_pos in self.percepts["neighbor_wastes"]:
+            waste = self.model.get_agent_by_id(waste_id)
+            if (
+                waste.waste_color == self.knowledge["carried_waste_type"]
+                or self.knowledge["carried_waste_type"] is None
+            ) and waste.waste_color <= self.knowledge["zone_type"]:
+                filtered_wastes.append(waste_id)
+        if filtered_wastes and self.knowledge["carried_waste_amount"] < 2:
+            return "pick_waste"
 
         # Default action: Move
+        print("Moving")
         return "move"
 
     def step_agent(self):
         self.update()
-        action = self.deleberate()
+        action = self.deliberate()
         self.percepts = self.model.do(self, action)
