e0cee3e2414afe369dc0ffde96a018ac5d9a602a
Idriss Mo <idrissmortadi@gmail.com>
2025-03-28 14:14:08 +0100

    Refactor agent portrayal logic and update model parameters for improved clarity and functionality

--- src/server.py
+++ src/server.py
@@ -17,13 +17,13 @@ def agent_portrayal(agent: mesa.Agent):
         }
         WASTES_MARKER_MAP = {
             0: "o",
-            1: "s",
-            2: "D",
+            1: "D",
+            2: "*",
         }
 
         portrayal = {
-            "marker": WASTES_MARKER_MAP.get(agent.waste_color, "circle"),
-            "color": WASTES_COLOR_MAP.get(agent.waste_color, "D"),
+            "marker": WASTES_MARKER_MAP.get(agent.waste_color, "o"),
+            "color": WASTES_COLOR_MAP.get(agent.waste_color, "black"),
             "zorder": 101,
             "size": 70,
         }
@@ -36,14 +36,23 @@ def agent_portrayal(agent: mesa.Agent):
         portrayal = {
             "color": AGENT_COLOR_MAP.get(agent.zone_type, "purple"),
             "zorder": 100,
+            "size": 500,
         }
     elif isinstance(agent, Zone):
-        return {
-            "marker": "s",
-            "color": COLORS_MAP[agent.zone_type],
-            "zorder": 99,
-            "size": 1300,
-        }
+        if agent.is_drop_zone:
+            portrayal = {
+                "marker": "s",
+                "color": "black",
+                "zorder": 99,
+                "size": 1000,
+            }
+        else:
+            return {
+                "marker": "s",
+                "color": COLORS_MAP[agent.zone_type],
+                "zorder": 99,
+                "size": 1000,
+            }
     return portrayal
 
 
@@ -98,37 +107,37 @@ model_params = {
     },
     "width": {
         "type": "SliderInt",
-        "value": 9,
+        "value": 10,
         "label": "Grid width:",
-        "min": 9,
+        "min": 10,
         "max": 100,
         "step": 1,
     },
     "height": {
         "type": "SliderInt",
-        "value": 9,
+        "value": 10,
         "label": "Grid height:",
-        "min": 9,
+        "min": 10,
         "max": 100,
         "step": 1,
     },
 }
-money_model = Environment(
-    green_agents=1,
-    yellow_agents=1,
-    red_agents=1,
-    green_wastes=5,
-    yellow_wastes=3,
-    red_wastes=2,
-    width=9,
-    height=9,
+model = Environment(
+    green_agents=model_params["green_agents"]["value"],
+    yellow_agents=model_params["yellow_agents"]["value"],
+    red_agents=model_params["red_agents"]["value"],
+    green_wastes=model_params["green_wastes"]["value"],
+    yellow_wastes=model_params["yellow_wastes"]["value"],
+    red_wastes=model_params["red_wastes"]["value"],
+    width=model_params["width"]["value"],
+    height=model_params["height"]["value"],
 )
 
 SpaceGraph = make_space_component(agent_portrayal)
 WastesPlot = make_plot_component(["green_wastes", "yellow_wastes", "red_wastes"])
 
 page = SolaraViz(
-    money_model,
+    model,
     components=[SpaceGraph, WastesPlot],
     model_params=model_params,
     name="Simple",
